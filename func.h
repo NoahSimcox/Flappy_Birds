81b4c309c97b34232a8aae28dc000c535741dde8
NoahSimcox <nssimcox@gmail.com>
2024-01-23 18:43:09 -0500

    first commit

---
+++ func.h
@@ -0,0 +1,4 @@
+#include <ti/screen.h>
+#include <ti/getcsc.h>
+#include <stdlib.h>
+
---
+++ main.c
@@ -0,0 +1,21 @@
+#include <ti/screen.h>
+#include <ti/getcsc.h>
+#include <stdlib.h>
+
+void end();
+bool step();
+void draw();
+
+void main() {
+    begin(); // No rendering allowed!
+    gfx_Begin();
+    gfx_SetDrawBuffer(); // Draw to the buffer to avoid rendering artifacts
+
+    while (step()) { // No rendering allowed in step!
+        draw(); // As little non-rendering logic as possible
+        gfx_SwapDraw(); // Queue the buffered frame to be displayed
+    }
+
+    gfx_End();
+    end();
+}

989a59e010cd249e7ca60cf88f9b2281f7669efe
Noah <nssimcox@gmail.com>
2024-01-24 16:25:55 -0500

    getting started

--- func.h
+++ func.h
@@ -2,3 +2,6 @@
 #include <ti/getcsc.h>
 #include <stdlib.h>
 
+#define GRAVITY -1.5f
+#define INIT_VEL 7.5f
+#define MAX_VEL 15.0f
--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <ti/screen.h>
 #include <ti/getcsc.h>
 #include <stdlib.h>
+#include <graphx.h>
+#include <keypadc.h>
+#include <func.h>
 
-void end();
-bool step();
-void draw();
+int Y = GFX_LCD_HEIGHT / 2;
+int X = GFX_LCD_WIDTH / 4;
+float velY;
 
 void main() {
     begin(); // No rendering allowed
@@ -12,10 +15,31 @@ void main() {
     gfx_SetDrawBuffer(); // Draw to the buffer to avoid rendering artifacts
 
     while (step()) { // No rendering allowed in step!
-        draw(); // As little non-rendering logic as possible
+        draw(); // as little non-render logic as possible
         gfx_SwapDraw(); // Queue the buffered frame to be displayed
     }
 
     gfx_End();
-    end();
+}
+
+
+bool step(){
+
+    kb_Scan();
+
+    if (kb_Data[6] & kb_Clear)
+        return false;
+    
+    if ((kb_Data[6] & kb_Enter) && velY < MAX_VEL)
+        velY += INIT_VEL;
+
+    velY += GRAVITY; // apply gravity every tick
+
+    return true;
+}
+
+
+void draw(){
+
+    
 }
